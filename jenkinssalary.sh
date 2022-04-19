6c2ce88e6cb5ffbb3160ecd107f86b1e1e38919a
BasavarajK1 <102715332+BasavarajK1@users.noreply.github.com>
2022-04-19 10:04:32 +0530

    Create jenkinssalary.sh

---
+++ jenkinssalary.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "The basic salary is: "
+#read basic
+$basic
+dp=$(expr 50 \* $basic)
+dp=$(expr $dp / 100)
+echo "dp is $dp"
+#x=(basic + dp)
+x=$(expr $basic + $dp)
+da=$(expr 35 \* $x)
+da=$(expr $da / 100)
+echo "da is $da"
+hra=$(expr 8 \* $x)
+hra=$(expr $hra / 100)
+echo "hra is $hra"
+ma=$(expr 3 \* $x)
+ma=$(expr $ma / 100)
+echo "ma is $ma"
+pf=$(expr 10 \* $x)
+pf=$(expr $pf / 100)
+echo "pf is $pf"
+salary=$(expr $basic + $dp + $da + $hra + $ma - $pf)
+echo "Salary is $salary"
