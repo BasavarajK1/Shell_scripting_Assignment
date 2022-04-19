cf441dc7a3f2a93eead306143a90bb9e6c10daff
BasavarajK1 <102715332+BasavarajK1@users.noreply.github.com>
2022-04-19 12:50:37 +0530

    Create calculatorjenkins.sh

---
+++ calculatorjenkins.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Enter two numbers: "
+#read a
+$a
+#read b
+$b
+echo "Enter operand: "
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+#read op
+$op
+if [ $op == 1 ]
+then 
+add=$(expr $a + $b)
+echo $add
+elif [ $op == 2 ]
+then
+sub=$(expr $a - $b)
+echo $sub
+elif [ $op == 3 ]
+then
+multi=$(expr $a \* $b)
+echo $multi
+elif [ $op == 4 ]
+then
+div=$(expr $a / $b)
+echo $div
+fi
